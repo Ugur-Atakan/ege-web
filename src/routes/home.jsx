bb4c2229fcf708839a60242229813325990785cc
Cihan Sahin <cihan.s@safe.security>
2022-12-06 10:59:20 +0300

    clean up home

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,13 +1,10 @@
 
-import { Navbar } from '../components/Navbar';
 import Hero from '../components/Hero'
 import PrimaryFeatures from '../components/PrimaryFeatures';
 import SecondaryFeatures from '../components/SecondaryFeatures';
 import CallToAction from '../components/CallToAction';
 import WhyUs from '../components/WhyUs';
 import Testimonials from '../components/Testimonials';
-import Pricing from '../components/Pricing';
-import Faqs from '../components/Faqs';
 import Customers from '../components/Customers';
 import Footer from '../components/Footer';
 import React from 'react';
