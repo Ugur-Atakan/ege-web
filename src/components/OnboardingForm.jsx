db2be93aab6baaa346145eb3282a7a35d39102eb
Cihan Sahin <cihan@opsbeacon.com>
2022-12-30 01:24:00 +0300

    Added terms/conditions links to first Submit button Converted State and Country to dropdowns

--- src/components/OnboardingForm.jsx
+++ src/components/OnboardingForm.jsx
@@ -6,6 +6,307 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
+const states = [
+    { id: 1, name: 'Alabama' },
+    { id: 2, name: 'Alaska' },
+    { id: 3, name: 'Arizona' },
+    { id: 4, name: 'Arkansas' },
+    { id: 5, name: 'California' },
+    { id: 6, name: 'Colorado' },
+    { id: 7, name: 'Connecticut' },
+    { id: 8, name: 'Delaware' },
+    { id: 9, name: 'Florida' },
+    { id: 10, name: 'Georgia' },
+    { id: 11, name: 'Hawaii' },
+    { id: 12, name: 'Idaho' },
+    { id: 13, name: 'Illinois' },
+    { id: 14, name: 'Indiana' },
+    { id: 15, name: 'Iowa' },
+    { id: 16, name: 'Kansas' },
+    { id: 17, name: 'Kentucky' },
+    { id: 18, name: 'Louisiana' },
+    { id: 19, name: 'Maine' },
+    { id: 20, name: 'Maryland' },
+    { id: 21, name: 'Massachusetts' },
+    { id: 22, name: 'Michigan' },
+    { id: 23, name: 'Minnesota' },
+    { id: 24, name: 'Mississippi' },
+    { id: 25, name: 'Missouri' },
+    { id: 26, name: 'Montana' },
+    { id: 27, name: 'Nebraska' },
+    { id: 28, name: 'Nevada' },
+    { id: 29, name: 'New Hampshire' },
+    { id: 30, name: 'New Jersey' },
+    { id: 31, name: 'New Mexico' },
+    { id: 32, name: 'New York' },
+    { id: 33, name: 'North Carolina' },
+    { id: 34, name: 'North Dakota' },
+    { id: 35, name: 'Ohio' },
+    { id: 36, name: 'Oklahoma' },
+    { id: 37, name: 'Oregon' },
+    { id: 38, name: 'Pennsylvania' },
+    { id: 39, name: 'Rhode Island' },
+    { id: 40, name: 'South Carolina' },
+    { id: 41, name: 'South Dakota' },
+    { id: 42, name: 'Tennessee' },
+    { id: 43, name: 'Texas' },
+    { id: 44, name: 'Utah' },
+    { id: 45, name: 'Vermont' },
+    { id: 46, name: 'Virginia' },
+    { id: 47, name: 'Washington' },
+    { id: 48, name: 'Washington DC' },
+    { id: 49, name: 'West Virginia' },
+    { id: 50, name: 'Wisconsin' },
+    { id: 51, name: 'Wyoming' }
+  ]
+
+const countries = [
+    { id: 1, name: 'United States' },
+    { id: 2, name: 'Canada' },
+    { id: 3, name: 'France' },
+    { id: 4, name: 'Afghanistan' },
+    { id: 5, name: 'Albania' },
+    { id: 6, name: 'Algeria' },
+    { id: 7, name: 'American Samoa' },
+    { id: 8, name: 'Andorra' },
+    { id: 9, name: 'Angola' },
+    { id: 10, name: 'Anguilla' },
+    { id: 11, name: 'Antarctica' },
+    { id: 12, name: 'Antigua and Barbuda' },
+    { id: 13, name: 'Argentina' },
+    { id: 14, name: 'Armenia' },
+    { id: 15, name: 'Aruba' },
+    { id: 16, name: 'Australia' },
+    { id: 17, name: 'Austria' },
+    { id: 18, name: 'Azerbaijan' },
+    { id: 19, name: 'Bahamas' },
+    { id: 20, name: 'Bahrain' },
+    { id: 21, name: 'Bangladesh' },
+    { id: 22, name: 'Barbados' },
+    { id: 23, name: 'Belarus' },
+    { id: 24, name: 'Belgium' },
+    { id: 25, name: 'Belize' },
+    { id: 26, name: 'Benin' },
+    { id: 27, name: 'Bermuda' },
+    { id: 28, name: 'Bhutan' },
+    { id: 29, name: 'Bolivia, Plurinational State of' },
+    { id: 30, name: 'Bonaire, Sint Eustatius and Saba' },
+    { id: 31, name: 'Bosnia and Herzegovina' },
+    { id: 32, name: 'Botswana' },
+    { id: 33, name: 'Bouvet Island' },
+    { id: 34, name: 'Brazil' },
+    { id: 35, name: 'British Indian Ocean Territory' },
+    { id: 36, name: 'Brunei Darussalam' },
+    { id: 37, name: 'Bulgaria' },
+    { id: 38, name: 'Burkina Faso' },
+    { id: 39, name: 'Burundi' },
+    { id: 40, name: 'Cambodia' },
+    { id: 41, name: 'Cameroon' },
+    { id: 42, name: 'Cape Verde' },
+    { id: 43, name: 'Cayman Islands' },
+    { id: 44, name: 'Central African Republic' },
+    { id: 45, name: 'Chad' },
+    { id: 46, name: 'Chile' },
+    { id: 47, name: 'China' },
+    { id: 48, name: 'Christmas Island' },
+    { id: 49, name: 'Cocos (Keeling) Islands' },
+    { id: 50, name: 'Colombia' },
+    { id: 51, name: 'Comoros' },
+    { id: 52, name: 'Congo' },
+    { id: 53, name: 'Congo, the Democratic Republic of the' },
+    { id: 54, name: 'Cook Islands' },
+    { id: 55, name: 'Costa Rica' },
+    { id: 56, name: 'Côte d\'Ivoire' },
+    { id: 57, name: 'Croatia' },
+    { id: 58, name: 'Cuba' },
+    { id: 59, name: 'Curaçao' },
+    { id: 60, name: 'Cyprus' },
+    { id: 61, name: 'Czech Republic' },
+    { id: 62, name: 'Denmark' },
+    { id: 63, name: 'Djibouti' },
+    { id: 64, name: 'Dominica' },
+    { id: 65, name: 'Dominican Republic' },
+    { id: 66, name: 'Ecuador' },
+    { id: 67, name: 'Egypt' },
+    { id: 68, name: 'El Salvador' },
+    { id: 69, name: 'Equatorial Guinea' },
+    { id: 70, name: 'Eritrea' },
+    { id: 71, name: 'Estonia' },
+    { id: 72, name: 'Ethiopia' },
+    { id: 73, name: 'Falkland Islands (Malvinas)' },
+    { id: 74, name: 'Faroe Islands' },
+    { id: 75, name: 'Fiji' },
+    { id: 76, name: 'Finland' },
+    { id: 78, name: 'French Guiana' },
+    { id: 79, name: 'French Polynesia' },
+    { id: 80, name: 'French Southern Territories' },
+    { id: 81, name: 'Gabon' },
+    { id: 82, name: 'Gambia' },
+    { id: 83, name: 'Georgia' },
+    { id: 84, name: 'Germany' },
+    { id: 85, name: 'Ghana' },
+    { id: 86, name: 'Gibraltar' },
+    { id: 87, name: 'Greece' },
+    { id: 88, name: 'Greenland' },
+    { id: 89, name: 'Grenada' },
+    { id: 90, name: 'Guadeloupe' },
+    { id: 91, name: 'Guam' },
+    { id: 92, name: 'Guatemala' },
+    { id: 93, name: 'Guernsey' },
+    { id: 94, name: 'Guinea' },
+    { id: 95, name: 'Guinea-Bissau' },
+    { id: 96, name: 'Guyana' },
+    { id: 97, name: 'Haiti' },
+    { id: 98, name: 'Heard Island and McDonald Islands' },
+    { id: 99, name: 'Holy See (Vatican City State)' },
+    { id: 100, name: 'Honduras' },
+    { id: 101, name: 'Hong Kong' },
+    { id: 102, name: 'Hungary' },
+    { id: 103, name: 'Iceland' },
+    { id: 104, name: 'India' },
+    { id: 105, name: 'Indonesia' },
+    { id: 106, name: 'Iran, Islamic Republic of' },
+    { id: 107, name: 'Iraq' },
+    { id: 108, name: 'Ireland' },
+    { id: 109, name: 'Japan' },
+    { id: 110, name: 'Jersey' },
+    { id: 111, name: 'Jordan' },
+    { id: 112, name: 'Kazakhstan' },
+    { id: 113, name: 'Kenya' },
+    { id: 114, name: 'Kiribati' },
+    { id: 115, name: "Korea, Democratic People's Republic of" },
+    { id: 116, name: 'Korea, Republic of' },
+    { id: 117, name: 'Kuwait' },
+    { id: 118, name: 'Kyrgyzstan' },
+    { id: 119, name: "Lao People's Democratic Republic" },
+    { id: 120, name: 'Latvia' },
+    { id: 121, name: 'Lebanon' },
+    { id: 122, name: 'Lesotho' },
+    { id: 123, name: 'Liberia' },
+    { id: 124, name: 'Libya' },
+    { id: 125, name: 'Liechtenstein' },
+    { id: 126, name: 'Lithuania' },
+    { id: 127, name: 'Luxembourg' },
+    { id: 128, name: 'Macao' },
+    { id: 129, name: 'Macedonia, the former Yugoslav Republic of' },
+    { id: 130, name: 'Madagascar' },
+    { id: 131, name: 'Malawi' },
+    { id: 132, name: 'Malaysia' },
+    { id: 133, name: 'Maldives' },
+    { id: 134, name: 'Mali' },
+    { id: 135, name: 'Malta' },
+    { id: 136, name: 'Marshall Islands' },
+    { id: 137, name: 'Martinique' },
+    { id: 138, name: 'Mauritania' },
+    { id: 139, name: 'Mauritius' },
+    { id: 140, name: 'Mayotte' },
+    { id: 141, name: 'Mexico' },
+    { id: 142, name: 'Micronesia, Federated States of' },
+    { id: 143, name: 'Moldova, Republic of' },
+    { id: 144, name: 'Monaco' },
+    { id: 145, name: 'Mongolia' },
+    { id: 146, name: 'Montenegro' },
+    { id: 147, name: 'Montserrat' },
+    { id: 148, name: 'Morocco' },
+    { id: 149, name: 'Mozambique' },
+    { id: 150, name: 'Myanmar' },
+    { id: 151, name: 'Namibia' },
+    { id: 152, name: 'Nauru' },
+    { id: 153, name: 'Nepal' },
+    { id: 154, name: 'Netherlands' },
+    { id: 155, name: 'New Caledonia' },
+    { id: 156, name: 'New Zealand' },
+    { id: 157, name: 'Nicaragua' },
+    { id: 158, name: 'Niger' },
+    { id: 159, name: 'Nigeria' },
+    { id: 160, name: 'Niue' },
+    { id: 161, name: 'Norfolk Island' },
+    { id: 162, name: 'Northern Mariana Islands' },
+    { id: 163, name: 'Norway' },
+    { id: 164, name: 'Oman' },
+    { id: 165, name: 'Pakistan' },
+    { id: 166, name: 'Palau' },
+    { id: 167, name: 'Palestinian Territory, Occupied' },
+    { id: 168, name: 'Panama' },
+    { id: 169, name: 'Papua New Guinea' },
+    { id: 170, name: 'Paraguay' },
+    { id: 171, name: 'Peru' },
+    { id: 172, name: 'Philippines' },
+    { id: 173, name: 'Pitcairn' },
+    { id: 174, name: 'Poland' },
+    { id: 175, name: 'Portugal' },
+    { id: 176, name: 'Puerto Rico' },
+    { id: 177, name: 'Qatar' },
+    { id: 178, name: 'Réunion' },
+    { id: 179, name: 'Romania' },
+    { id: 180, name: 'Russian Federation' },
+    { id: 181, name: 'Rwanda' },
+    { id: 182, name: 'Saint Barthélemy' },
+    { id: 183, name: 'Saint Helena, Ascension and Tristan da Cunha' },
+    { id: 184, name: 'Saint Kitts and Nevis' },
+    { id: 185, name: 'Saint Lucia' },
+    { id: 186, name: 'Saint Martin (French part)' },
+    { id: 187, name: 'Saint Pierre and Miquelon' },
+    { id: 188, name: 'Saint Vincent and the Grenadines' },
+    { id: 189, name: 'Samoa' },
+    { id: 190, name: 'San Marino' },
+    { id: 191, name: 'Sao Tome and Principe' },
+    { id: 192, name: 'Saudi Arabia' },
+    { id: 193, name: 'Senegal' },
+    { id: 194, name: 'Serbia' },
+    { id: 195, name: 'Seychelles' },
+    { id: 196, name: 'Sierra Leone' },
+    { id: 197, name: 'Singapore' },
+    { id: 198, name: 'Sint Maarten (Dutch part)' },
+    { id: 199, name: 'Slovakia' },
+    { id: 200, name: 'Slovenia' },
+    { id: 201, name: 'Solomon Islands' },
+    { id: 202, name: 'Somalia' },
+    { id: 203, name: 'South Africa' },
+    { id: 204, name: 'South Georgia and the South Sandwich Islands' },
+    { id: 205, name: 'South Sudan' },
+    { id: 206, name: 'Spain' },
+    { id: 207, name: 'Sri Lanka' },
+    { id: 208, name: 'Sudan' },
+    { id: 209, name: 'Suriname' },
+    { id: 210, name: 'Svalbard and Jan Mayen' },
+    { id: 211, name: 'Swaziland' },
+    { id: 212, name: 'Sweden' },
+    { id: 213, name: 'Switzerland' },
+    { id: 214, name: 'Syrian Arab Republic' },
+    { id: 215, name: 'Taiwan, Province of China' },
+    { id: 216, name: 'Tajikistan' },
+    { id: 217, name: 'Tanzania, United Republic of' },
+    { id: 218, name: 'Thailand' },
+    { id: 219, name: 'Timor-Leste' },
+    { id: 220, name: 'Togo' },
+    { id: 221, name: 'Tokelau' },
+    { id: 222, name: 'Tonga' },
+    { id: 223, name: 'Trinidad and Tobago' },
+    { id: 224, name: 'Tunisia' },
+    { id: 225, name: 'Turkey' },
+    { id: 226, name: 'Turkmenistan' },
+    { id: 227, name: 'Turks and Caicos Islands' },
+    { id: 228, name: 'Tuvalu' },
+    { id: 229, name: 'Uganda' },
+    { id: 230, name: 'Ukraine' },
+    { id: 231, name: 'United Arab Emirates' },
+    { id: 232, name: 'United Kingdom' },
+    { id: 233, name: 'United States Minor Outlying Islands' },
+    { id: 234, name: 'Uruguay' },
+    { id: 235, name: 'Uzbekistan' },
+    { id: 236, name: 'Vanuatu' },
+    { id: 237, name: 'Venezuela, Bolivarian Republic of' },
+    { id: 238, name: 'Viet Nam' },
+    { id: 239, name: 'Virgin Islands, British' },
+    { id: 240, name: 'Virgin Islands, U.S.' },
+    { id: 241, name: 'Wallis and Futuna' },
+    { id: 242, name: 'Western Sahara' },
+    { id: 243, name: 'Yemen' },
+    { id: 244, name: 'Zambia' },
+    { id: 245, name: 'Zimbabwe' }
+]
+
 const OrderInformationPanel = ({ companyName, packageName, packagePrice}) => {
 
     return (
@@ -17,7 +318,7 @@ const OrderInformationPanel = ({ companyName, packageName, packagePrice}) => {
                             <h3 className="text-lg font-medium leading-6 text-gray-900">Selected Package: <span className="text-blue-700">{packageName} - ${packagePrice}</span></h3>
                         )}
                         {companyName && (
-                            <h3 className="text-lg font-medium leading-6 text-gray-900">Company Name: <span className="text-blue-700">{companyName}</span></h3>
+                            <h3 className="text-lg font-medium leading-6 text-gray-900">Selected Name: <span className="text-blue-700">{companyName}</span></h3>
                         )}
                     </div>
                     <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex sm:flex-shrink-0 sm:items-center">
@@ -102,7 +403,24 @@ const CompanyNameEmailForm = ({ setCompanyName }) => {
                         <input type="email" name="userEmail" id="userEmail" className="w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm" placeholder="you@example.com"></input>
                     </div>
                     <div className="mt-4">
-                        <button type="submit" className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-blue-600 px-16 py-3 font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Submit</button>
+                        <button type="submit" className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-blue-600 px-16 py-3 font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                            Select Company Name
+                        </button>
+                        <p className="text-xs leading-5 text-gray-500">
+                      By submitting this form, you agree to our{' '}
+                      <a href="#" className="font-medium text-gray-900 hover:underline">
+                        Terms
+                      </a>
+                      ,{' '}
+                      <a href="#" className="font-medium text-gray-900 hover:underline">
+                        Data Policy
+                      </a>{' '}
+                      and{' '}
+                      <a href="#" className="font-medium text-gray-900 hover:underline">
+                        Cookies Policy
+                      </a>
+                      .
+                    </p>
                     </div>
                 </div>
             </form>
@@ -251,16 +569,25 @@ const CompanyContactInfoForm = () => {
                             </div>
             
                             <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                            <label htmlFor="region" className="block text-sm font-medium text-gray-700">
-                                State / Province
-                            </label>
-                            <input
-                                type="text"
-                                name="region"
-                                id="region"
-                                autoComplete="address-level1"
-                                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
-                            />
+                                <label htmlFor="region" className="block text-sm font-medium text-gray-700">
+                                    State / Province
+                                </label>
+                                <select
+                                    name="region"
+                                    id="region"
+                                    autoComplete="address-level1"
+                                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                                >
+                                    <option value=""> Select State </option>
+                                    {states.map((state) => (
+                                        <option 
+                                        key={state.name} 
+                                        value={state.name}
+                                        >
+                                        {state.name}
+                                        </option>
+                                    ))}
+                                </select>
                             </div>
             
                             <div className="col-span-6 sm:col-span-3 lg:col-span-2">
@@ -286,9 +613,16 @@ const CompanyContactInfoForm = () => {
                                 autoComplete="country-name"
                                 className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
                             >
-                                <option>United States</option>
-                                <option>Canada</option>
-                                <option>Mexico</option>
+                                <option value="">Select Country</option>
+                                {countries.map((country) => (
+                                        <option 
+                                        key={country.name} 
+                                        value={country.name}
+                                        id={country.id}
+                                        >
+                                        {country.name}
+                                        </option>
+                                    ))}
                             </select>
                             </div>
 
@@ -303,7 +637,7 @@ const CompanyContactInfoForm = () => {
                 type="submit"
                 className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
             >
-                Save
+                Review and Pay
             </button>
             </div>
 
@@ -325,8 +659,8 @@ const OnboardingForm = () => {
         //Check if packageType is defined in local storage, if not redirect to /pricing
         let packageType = localStorage.getItem('packageType')
         if (!packageType) {
-            console.log("packageType not defined, redirecting to /pricing")
-            window.location.href = "/pricing"
+            console.log("packageType not defined, redirecting to /pricing/form-my-company")
+            window.location.href = "/pricing/form-my-company"
         }
 
         let onboardingId = localStorage.getItem('onboardingId')
