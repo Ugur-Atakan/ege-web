8c2e6e05ad85dd59244fb5f2d661faf1b7cabed1
Cihan Sahin <cihan@opsbeacon.com>
2023-01-04 14:07:10 +0300

    get state names and prices from backend

--- src/components/PricingFormMyCompany.jsx
+++ src/components/PricingFormMyCompany.jsx
@@ -4,59 +4,8 @@ import { useState, useEffect, useRef } from 'react'
 
 import React from 'react'
 
-const states = [
-  { id: 1, name: 'Alabama' },
-  { id: 2, name: 'Alaska' },
-  { id: 3, name: 'Arizona' },
-  { id: 4, name: 'Arkansas' },
-  { id: 5, name: 'California' },
-  { id: 6, name: 'Colorado' },
-  { id: 7, name: 'Connecticut' },
-  { id: 8, name: 'Delaware' },
-  { id: 9, name: 'Florida' },
-  { id: 10, name: 'Georgia' },
-  { id: 11, name: 'Hawaii' },
-  { id: 12, name: 'Idaho' },
-  { id: 13, name: 'Illinois' },
-  { id: 14, name: 'Indiana' },
-  { id: 15, name: 'Iowa' },
-  { id: 16, name: 'Kansas' },
-  { id: 17, name: 'Kentucky' },
-  { id: 18, name: 'Louisiana' },
-  { id: 19, name: 'Maine' },
-  { id: 20, name: 'Maryland' },
-  { id: 21, name: 'Massachusetts' },
-  { id: 22, name: 'Michigan' },
-  { id: 23, name: 'Minnesota' },
-  { id: 24, name: 'Mississippi' },
-  { id: 25, name: 'Missouri' },
-  { id: 26, name: 'Montana' },
-  { id: 27, name: 'Nebraska' },
-  { id: 28, name: 'Nevada' },
-  { id: 29, name: 'New Hampshire' },
-  { id: 30, name: 'New Jersey' },
-  { id: 31, name: 'New Mexico' },
-  { id: 32, name: 'New York' },
-  { id: 33, name: 'North Carolina' },
-  { id: 34, name: 'North Dakota' },
-  { id: 35, name: 'Ohio' },
-  { id: 36, name: 'Oklahoma' },
-  { id: 37, name: 'Oregon' },
-  { id: 38, name: 'Pennsylvania' },
-  { id: 39, name: 'Rhode Island' },
-  { id: 40, name: 'South Carolina' },
-  { id: 41, name: 'South Dakota' },
-  { id: 42, name: 'Tennessee' },
-  { id: 43, name: 'Texas' },
-  { id: 44, name: 'Utah' },
-  { id: 45, name: 'Vermont' },
-  { id: 46, name: 'Virginia' },
-  { id: 47, name: 'Washington' },
-  { id: 48, name: 'Washington DC' },
-  { id: 49, name: 'West Virginia' },
-  { id: 50, name: 'Wisconsin' },
-  { id: 51, name: 'Wyoming' }
-]
+import axios from "axios";
+
 
 const packageDetails = {
 "llc1": {
@@ -79,7 +28,6 @@ const packageDetails = {
         ],
     cta: 'Select LLC Gold',
     mostPopular: false,
-    id: 'llc1'
 },
 "llc2": {
     title: 'Registate LLC Premium',
@@ -97,27 +45,8 @@ const packageDetails = {
     ],
     cta: 'Select LLC Premium',
     mostPopular: true,
-    id: 'llc2'
 },
-"llc3": {
-  title: 'Registate LLC Premium',
-  frequency: '',
-  description: 'Expedited Filing (1-3 business days).',
-  description2: 'Everything you need to diligently operate and manage your company.',
-  features: [
-  'Everything in Gold',
-  '1 Year Free Compliance Calendar',
-  'Assisting payment of franchise tax and annual report filing fees',
-  'Operating Agreement',
-  'LLC Membership Certificate',
-  'LLC Interest Purchase Agreement',
-  'Business Contract Templates',
-  ],
-  cta: 'Select LLC Premium',
-  mostPopular: true,
-  id: 'llc3'
-},
-"corporation1": {
+"corp1": {
     title: 'Starter',
     price: 349,
     frequency: '',
@@ -140,9 +69,8 @@ const packageDetails = {
     ],
     cta: 'Select Starter',
     mostPopular: false,
-    id: 'corporation1'
 },
-"corporation2": {
+"corp2": {
     title: 'Start Up',
     price: 769,
     frequency: '',
@@ -162,9 +90,8 @@ const packageDetails = {
     ],
     cta: 'Select Start Up',
     mostPopular: true,
-    id: 'corporation2'
 },
-"corporation3": {
+"corp3": {
     title: 'Scale Up',
     price: 1769,
     frequency: '',
@@ -184,224 +111,7 @@ const packageDetails = {
     ],
     cta: 'Select Scale Up',
     mostPopular: false,
-    id: 'corporation3'
 }
-}
-
-const pricing= {
-  "Alabama" : {
-      "Corporation": [ 395, 595],
-      "LLC": [ 395, 595]
-  },
-  "Alaska" : {
-      "Corporation": [ 409, 609],
-      "LLC": [ 409, 609]
-  },
-  "Arizona" : {
-      "Corporation": [ 250, 450],
-      "LLC": [ 276, 476]
-  },
-  "Arkansas" : {
-      "Corporation": [ 198, 398],
-      "LLC": [ 198, 398]
-  },
-  "California" : {
-      "Corporation": [ 157, 557, 1557],
-      "LLC": [ 157, 357]
-  },
-  "Colorado" : {
-      "Corporation": [ 189, 389],
-      "LLC": [ 139, 339]
-  },
-  "Connecticut" : {
-      "Corporation": [ 409, 609],
-      "LLC": [ 276, 476]
-  },
-  "Delaware" : {
-      "Corporation": [ 349, 649, 1449],
-      "LLC": [ 322, 622]
-  },
-  "Florida" : {
-      "Corporation": [ 210, 410],
-      "LLC": [ 266, 466]
-  },
-  "Georgia" : {
-      "Corporation": [ 333, 533],
-      "LLC": [ 333, 533]
-  },
-  "Hawaii" : {
-      "Corporation": [ 230, 430],
-      "LLC": [ 230, 430]
-  },
-  "Idaho" : {
-      "Corporation": [ 261, 461],
-      "LLC": [ 261, 461]
-  },
-  "Illinois" : {
-      "Corporation": [ 349, 769],
-      "LLC": [ 299, 599]
-  },
-  "Indiana" : {
-      "Corporation": [ 441, 641],
-      "LLC": [ 416, 616]
-  },
-  "Iowa" : {
-      "Corporation": [ 203, 403],
-      "LLC": [ 203, 403]
-  },
-  "Kansas" : {
-      "Corporation": [ 244, 444],
-      "LLC": [ 323, 523]
-  },
-  "Kentucky" : {
-      "Corporation": [ 209, 409],
-      "LLC": [ 193, 393]
-  },
-  "Louisiana" : {
-      "Corporation": [ 286, 486],
-      "LLC": [ 286, 486]
-  },
-  "Maine" : {
-      "Corporation": [ 356, 556],
-      "LLC": [ 387, 587]
-  },
-  "Maryland" : {
-      "Corporation": [ 377, 577],
-      "LLC": [ 355, 555]
-  },
-  "Massachusetts" : {
-      "Corporation": [ 425, 625],
-      "LLC": [ 688, 888]
-  },  
-  "Michigan" : {
-      "Corporation": [ 214, 414],
-      "LLC": [ 203, 403]
-  },
-  "Minnesota" : {
-      "Corporation": [ 312, 512],
-      "LLC": [ 203, 403]
-  },
-  "Mississippi" : {
-      "Corporation": [ 349, 769],
-      "LLC": [ 299, 599]
-  },
-  "Missouri" : {
-      "Corporation": [ 208, 408],
-      "LLC": [ 208, 408]
-  },
-  "Montana" : {
-      "Corporation": [ 245, 445],
-      "LLC": [ 245, 445]
-  },
-  "Nebraska" : {
-      "Corporation": [ 258, 458],
-      "LLC": [ 264, 464]
-  },
-  "Nevada" : {
-      "Corporation": [ 884, 1084],
-      "LLC": [ 575, 775]
-  },
-  "New Hampshire" : {
-      "Corporation": [ 312, 512],
-      "LLC": [ 288, 488]
-  },
-  "New Jersey" : {
-      "Corporation": [ 286, 486],
-      "LLC": [ 286, 486]
-  },
-  "New Mexico" : {
-      "Corporation": [ 241, 441],
-      "LLC": [ 189, 389]
-  },
-  "New York" : {
-      "Corporation": [ 286, 486, 999],
-      "LLC": [ 363, 563]
-  },
-  "North Carolina" : {
-      "Corporation": [ 284, 484],
-      "LLC": [ 284, 484]
-  },
-  "North Dakota" : {
-      "Corporation": [ 281, 481],
-      "LLC": [ 317, 517]
-  },
-  "Ohio" : {
-      "Corporation": [ 281, 481],
-      "LLC": [ 254, 454]
-  },
-  "Oklahoma" : {
-      "Corporation": [ 206, 406],
-      "LLC": [ 259, 459]
-  },
-  "Oregon" : {
-      "Corporation": [ 241, 441],
-      "LLC": [ 241, 441]
-  },
-  "Pennsylvania" : {
-      "Corporation": [ 287, 487],
-      "LLC": [ 287, 487]
-  },
-  "Rhode Island" : {
-      "Corporation": [ 397, 597],
-      "LLC": [ 313, 513]
-  },
-  "South Carolina" : {
-      "Corporation": [ 487, 687],
-      "LLC": [ 288, 488]
-  },
-  "South Dakota" : {
-      "Corporation": [ 306, 506],
-      "LLC": [ 306, 506]
-  },
-  "Tennessee" : {
-      "Corporation": [ 263, 463],
-      "LLC": [ 469, 669   ]
-  },
-  "Texas" : {
-      "Corporation": [ 447, 647],
-      "LLC": [ 447, 647]
-  },
-  "Utah" : {
-      "Corporation": [ 209, 409],
-      "LLC": [ 209, 409]
-  },
-  "Vermont" : {
-      "Corporation": [ 281, 481],
-      "LLC": [ 281, 481]
-  },
-  "Virginia" : {
-      "Corporation": [ 229, 429],
-      "LLC": [ 255, 455]
-  },
-  "Washington" : {
-      "Corporation": [ 368, 568],
-      "LLC": [ 368, 568]
-  },
-  "Washington DC" : {
-      "Corporation": [ 305, 505],
-      "LLC": [ 305, 505]
-  },
-  "West Virginia" : {
-      "Corporation": [ 281, 481],
-      "LLC": [ 306, 506]
-  },
-  "Wisconsin" : {
-      "Corporation": [ 255, 455],
-      "LLC": [ 286, 486]
-  },
-  "Wyoming" : {
-      "Corporation": [ 232, 432],
-      "LLC": [ 232, 432]
-  }
-}
-
-const companyTypes = [
-  { name: 'LLC'},
-  { name: 'Corporation'}
-]
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
 }
 
 function Plan(props) {
@@ -468,11 +178,11 @@ function Plan(props) {
       </ul>
       {/* if selected */}
       {props.featured && (
-      <button className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-white text-slate-900 hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-8" aria-label="Get started with the Small business plan for $15"  onClick={clickHandler.bind(this)} >Get started</button>
+      <button className="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-white text-slate-900 hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white mt-8" aria-label="Get started with the Small business plan for $15"  onClick={clickHandler.bind(this)} >{props.cta}</button>
       )}
       {/* if not selected */}
       {!props.featured && (
-        <button className="group inline-flex ring-1 items-center justify-center rounded-full py-2 px-4 text-sm focus:outline-none ring-slate-700 text-white hover:ring-slate-500 active:ring-slate-700 active:text-slate-400 focus-visible:outline-white mt-8" aria-label="Get started with the Starter plan for ${price}" onClick={clickHandler.bind(this)}>Get started</button>
+        <button className="group inline-flex ring-1 items-center justify-center rounded-full py-2 px-4 text-sm focus:outline-none ring-slate-700 text-white hover:ring-slate-500 active:ring-slate-700 active:text-slate-400 focus-visible:outline-white mt-8" aria-label="Get started with the Starter plan for ${price}" onClick={clickHandler.bind(this)}>{props.cta}</button>
       )}
     </section>
   )
@@ -482,18 +192,43 @@ export function Pricing() {
   let [companyState, setCompanyState] = useState("");
   let [companyType, setCompanyType] = useState("");
   let [showPricingPackages, setShowPricingPackages] = useState(false);
-  let [packagePrices, setPackagePrices] = useState()
+  let [packagePrices, setPackagePrices] = useState([])
+  let [states, setStates] = useState([])
+  let [companyTypes, setCompanyTypes] = useState([])
   const pricingPackagesRef = useRef(null);
+
 
 
   const updatePricing = () => {
     companyState = localStorage.getItem('companyState')
     companyType = localStorage.getItem('companyType')
-    console.log("updatePricing to", companyState, companyType)
 
     if (companyType && companyState) {
-      setPackagePrices(pricing[companyState][companyType])
-      console.log("packagePrices", pricing[companyState][companyType])
+      let companyTypeId = companyTypes.find(x => x.entityType === companyType).id
+      let companyStateId = states.find(x => x.state === companyState).id
+
+      let payload = {
+        stateId: companyStateId,
+        entityTypeId: companyTypeId
+      }
+
+      axios.post('https://api.registate.net/api/fe/prices', payload, {
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer D27F1E98-A574-4BC6-9090-033A85C4A0F6'
+      }})
+      .then(function (response) {
+        var jsonData = JSON.parse(JSON.stringify(response.data));
+        console.log("Response from prices API:")
+        //let prices = jsonData.map(x => x.orderPackagePrice.toString().slice(0, -2))
+        //console.log(prices)
+
+        //set package prices
+        setPackagePrices(jsonData)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     }
 
     if (companyState !== "" && companyType !== "") {
@@ -504,6 +239,34 @@ export function Pricing() {
   }
 
   useEffect(() => {
+    //get the states from backend api via axios
+    axios.get('https://api.registate.net/api/fe/states', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer D27F1E98-A574-4BC6-9090-033A85C4A0F6'
+    }})
+    .then(function (response) {
+      var jsonData = JSON.parse(JSON.stringify(response.data));
+      setStates(jsonData)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    //get the states from backend api via axios
+    axios.get('https://api.registate.net/api/settings/entityType', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer D27F1E98-A574-4BC6-9090-033A85C4A0F6'
+    }})
+    .then(function (response) {
+      var jsonData = JSON.parse(JSON.stringify(response.data));
+      setCompanyTypes(jsonData)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });  
+
     localStorage.setItem('companyState', companyState)
     localStorage.setItem('companyType', companyType)
     updatePricing()
@@ -577,10 +340,10 @@ export function Pricing() {
               <option value=""> Select State </option>
               {states.map((state) => (
                 <option 
-                  key={state.name} 
-                  value={state.name}
+                  key={state.id} 
+                  value={state.state}
                 >
-                  {state.name}
+                  {state.state}
                 </option>
               ))}
             </select>
@@ -598,10 +361,10 @@ export function Pricing() {
               <option value=""> Select Company Type </option>
               {companyTypes.map((type) => (
                 <option 
-                  key={type.name} 
-                  value={type.name} 
+                  key={type.id} 
+                  value={type.entityType} 
                 >
-                  {type.name}
+                  {type.entityType}
                 </option>
               ))}
             </select>
@@ -611,7 +374,7 @@ export function Pricing() {
         <div id="pricingPackages" className="lg:flex lg:justify-center mx-4 mt-16 grid max-w-2xl grid-cols-1 gap-y-10 sm:mx-auto lg:-mx-8 lg:max-w-none lg:grid-cols-3 xl:mx-0 xl:gap-x-8">
         {
           packagePrices.map((packagePrice, index) => {
-              let item = packageDetails[companyType.toLowerCase()+(index+1)]
+              let item = packageDetails[packagePrice.orderPackageId]
               if (companyState !== "Delaware") {
                 if (index === 0) {
                   //if item.features doesn't include Business Address Fee, add it
@@ -642,13 +405,13 @@ export function Pricing() {
                   }
                 }
               }
-              console.log("packageDetail[" + companyType.toLowerCase()+(index+1) + "]")
+              console.log("packageDetail[" + packagePrice.orderPackageId + "]")
               console.log(item)
               return (
                 <div key={item.id + index}>
                 <Plan
-                  name={item.title}
-                  price={packagePrice}
+                  name={packagePrice.orderPackage}
+                  price={packagePrice.orderPackagePrice.toString().slice(0, -2)}
                   description={item.description}
                   description2={item.description2}
                   packageId={companyState.toLocaleLowerCase() + "_" + item.id}
@@ -657,6 +420,7 @@ export function Pricing() {
                   featured={index === 1}
                   keyName={item.id + index}
                   packageType={item.id}
+                  cta={item.cta}
                 />
                 </div>
               )
