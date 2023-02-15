df1d65de2197ff783d3598efeb688c55c2c320a3
burakceylanz <burakbayarceylan@gmail.com>
2023-02-15 14:19:19 +0300

    removed I18nextBrowserLanguageDetector on lang file

--- src/locales/language.js
+++ src/locales/language.js
@@ -1,10 +1,8 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
 
 i18next
     .use(initReactI18next)
-    .use(I18nextBrowserLanguageDetector)
     .init({
         fallbackLng: 'en', // if you're using a language detector, do not define the lng option
         resources: {
