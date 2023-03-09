732df67abc79e7782349ddbdbff08bda85525b61
burakceylanz <burakbayarceylan@gmail.com>
2023-03-09 12:27:50 +0300

    updated the lang file on dissolution and cancellation page

--- src/routes/dissolution-and-cancellation.jsx
+++ src/routes/dissolution-and-cancellation.jsx
@@ -10,20 +10,22 @@ import Price from '../components/Price'
 import ExclusiveSolution from '../components/ExclusiveSolution'
 import Requirements from '../components/Requirements'
 import productData from '../assets/productData'
+import { useTranslation } from 'react-i18next'
 
 export default function DissolutionAndCancellation() {
+    const {t} = useTranslation();
     return (
         <main>
             <Navbar />
-            <LeftImgRightText img={topnotch} heading='Manage Your Business' title='Dissolution/Cancellation' desc='Are you winding-down the operations of your company? Then the dissolution is the procedure that you must start.' button='Get Started Now' href={`/products/${productData[13].link}/contact`} />
+            <LeftImgRightText img={topnotch} heading={t('dissolution_header1_uptitle_blue')} title={t('dissolution_header1_maintitle_black')} desc={t('dissolution_header1_text')} button={t('dissolution_header1_button')}  href={`/products/${productData[13].link}/contact`} />
             <Price/>
-            <Stats title="Step-By-Step Guide" icontitle1="Payment of due taxes" icondesc1="All outstanding taxes and penalties must be paid before filing for dissolution/cancellation. All delinquent annual reports must be filed as well. If you need assistance with either of these, please contact us." icontitle2="Provide us with the signed Certificate of Dissolution / Cancellation" icondesc2='A certificate must be filled out and signed by the company’s authorized signer.' icon1=<DocumentCheckIcon className="w-12" /> icon2=<CursorArrowRippleIcon className='w-12' /> icontitle3='Filing the documents' icon3=<NewspaperIcon className='w-12' /> icondesc3='We file the Certificate of Dissolution/Cancellation with the State of Delaware Division of Corporations. Once completed, you will be notified and receive the filed Certificate.' />
-            <LeftImgRightText img={whatweneed} desc=<ul>If you are to wind-down the operations of your corporation, then the dissolution is the procedure that you must start. If you wish to dissolve your Delaware Corporation formally, a Certificate of Dissolution must be prepared and filed. As for LLCs, a Certificate of Cancellation needs to be filed to terminate an LLC. But why should you go ahead with these formal procedures? Why not just leave the company as it is?
-            <li className='ml-8 py-1 list-disc'>This is a legal requirement by the State</li>
-            <li className='ml-8 py-1 list-disc'>To avoid fees and taxes</li>
-            <li className='ml-8 py-1 list-disc'>To put creditors on notice</li>By dissolving, the company’s responsibility for future annual Franchise Tax payments will also end. Company assets must be distributed among the owners based on the percentage of ownership complying with state law. Please do not forget to notify appropriate federal, state and local authorities that your company will be closing.</ul>/>
-            <ExclusiveSolution heading='Exclusive Solutions' title='Need a certified copy of a Certificate of Dissolution?' desc='If you are in need of additional copies of a Certificate of Dissolution from the State, we can help you getting certified copies!' rightdesc='We provide comprehensive services from compliance, filing, business set up, mailbox, bookkeeping, tax filing and more.' button='Learn More'/>
-            <Requirements title='Requirements' desc='The stockholders/members of the company must approve of the dissolution/cancellation.All taxes, fees and annual reports must be up to date and filed with the State. All due Franchise Taxes, including any late penalties and interest, must be paid before the company can be dissolved.' />
+            <Stats title={t('dissolution_header3_title')} icontitle1={t('dissolution_header3_icon1_title')} icondesc1={t('dissolution_header3_icon1_text')} icontitle2={t('dissolution_header3_icon2_title')} icondesc2={t('dissolution_header3_icon2_text')} icon1=<DocumentCheckIcon className="w-12" /> icon2=<CursorArrowRippleIcon className='w-12' /> icontitle3={t('dissolution_header3_icon3_title')} icon3=<NewspaperIcon className='w-12' /> icondesc3={t('dissolution_header3_icon3_text')} />
+            <LeftImgRightText img={whatweneed} desc=<ul>{t('dissolution_header4_text1')}
+            <li className='ml-8 py-1 list-disc'>{t('dissolution_header4_text2')}</li>
+            <li className='ml-8 py-1 list-disc'>{t('dissolution_header4_text3')}</li>
+            <li className='ml-8 py-1 list-disc'>{t('dissolution_header4_text4')}</li>{t('dissolution_header4_text5')}</ul>/>
+            <ExclusiveSolution heading={t('dissolution_header5_uptitle_blue')} title={t('dissolution_header5_maintitle_black')} desc={t('dissolution_header5_text_left')} rightdesc={t('dissolution_header5_text_right')} button={t('dissolution_header5_button')} />
+            <Requirements title={t('dissolution_header5_bottom_title_blue')} desc={t('dissolution_header5_bottom_title_black')} />
             <Footer />
         </main>
     )
