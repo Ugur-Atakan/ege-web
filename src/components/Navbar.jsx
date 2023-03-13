e03c39e863a15822ab3a080f6c71236929ee8b49
burakceylanz <burakbayarceylan@gmail.com>
2023-03-13 11:42:17 +0300

    added  id  for form my company button in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -342,6 +342,7 @@ export function Navbar() {
           <div className='hidden lg:flex items-center gap-4'>
             <a
               href="/pricing/form-my-company"
+              id="header-fmc"
               className={isScroll ? "shadow rounded-md border border-transparent font-medium text-white bg-blue-600 px-4 py-2 text-md transition-colors duration-1000" : "shadow rounded-md border border-blue-600 font-medium text-blue-600 px-4 py-2 text-md transition-colors duration-1000"}
             >
               {t('menu_top_right_handcorner_button')}
